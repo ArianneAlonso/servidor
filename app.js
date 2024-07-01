f7960d83db6f9aab9abd39f0ee4ed5cf49ca8dae
ArianneAlonso <ariannealonso8@gmail.com>
2024-07-01 16:15:41 -0300

    app get post

--- app.js
+++ app.js
@@ -2,11 +2,35 @@ const express = require("express")
 
 const app = express()
 
-let id = 1
+app.use(express.json())
+
+let libros = []
 
 app.get("/books")
 app.get("/books/:id")
-app.post("/books")
+
+app.post("/books", (req, res) => {
+    const { title, author, year } = req.body;
+
+    const id = new Date().getTime();
+
+    if (!title || !author || !year) {
+        return res.status(400).send("Faltan valores");
+    }
+
+    const newBook = {
+        "id": id,
+        "title": title,
+        "author": author,
+        "year": parseInt(year)
+    };
+
+    libros.push(newBook);
+
+    res.send("Libro creado");
+});
+
+
 app.put("/books/:id")
 
 app.listen(3000, console.log("server en puerto 3000"))
