094d74d6e73f6dfa719d5d03b190d19f05d15802
ArianneAlonso <ariannealonso8@gmail.com>
2024-07-01 15:26:39 -0300

    estructurar los puntos dados

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const express = ("express");
+const express = require("express")
 
-const book = book
+const app = express()
+
+let id = 1
+
+app.get("/books")
+app.get("/books/:id")
+app.post("/books")
+app.put("/books/:id")
+
+app.listen(3000, console.log("server en puerto 3000"))
